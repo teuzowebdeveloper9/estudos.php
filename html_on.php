6cf4b38da09686cf0d1619ea1087499f36bd18dc
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-02-17 00:21:32 -0300

    html_on.php

---
+++ html_on.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php on html</title>
+</head>
+<body>
+   <?php
+echo"com echo <br><br>";
+print "com print"
+
+        ?>
+</body>
+</html>
