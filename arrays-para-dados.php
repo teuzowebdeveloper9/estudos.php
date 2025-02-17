973a4d53e5a0962214cf8f2ccc37e3d75cb7118e
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-02-17 00:27:22 -0300

    arrays-para-dados.php

---
+++ arrays-para-dados.php
@@ -0,0 +1,16 @@
+<?php 
+     return[
+       'usuario1' => [
+          'email' =>'mateussoftwaredeveloper@gmail.com',
+          'senha' => password_hash('mateus123' , PASSWORD_DEFAULT)    
+       ],
+        'usuario2'=>[
+          'email' => 'mardonielsilva@gmail.com',
+           'senha' => password_hash('mardoniel123', PASSWORD_DEFAULT)
+        ],
+        'usuario3' =>[
+          'email' => 'andrinesilva@gmail.com',
+           'senha' => password_hash('andrine123', PASSWORD_DEFAULT)
+        ]
+        ];
+  
