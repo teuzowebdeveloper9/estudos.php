c8fc0e8f2387fab755ab9212860634334eff69c5
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-02-17 00:07:40 -0300

    switch.php

---
+++ switch.php
@@ -0,0 +1,18 @@
+<?php 
+
+$cor = "vermelho";
+
+switch  ($cor):
+    case "vermelho" :
+    echo "sua cor favorita é vermelho";
+    break;
+
+    case "verde":
+    echo  "sua cor favorita é verde";
+    break;
+
+    case "azul":
+    echo "sua cor favorita é azul";
+    break;
+    
+    endswitch;
