eba907512308710c8732812bafe46a2161fd4dfe
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-02-17 00:28:23 -0300

    testando.php

---
+++ testando.php
@@ -0,0 +1,19 @@
+<?php
+//opa
+$nome = "Rodrigo";
+$meu_cu = "largo";
+$penis = 23.5;
+
+echo "meu nome é $nome, meu cuzinho é $meu_cu e o menor penis que já chupei tinha $penis CM<br><br><br><br><br>";
+
+$bebida = "refrigerante";
+
+$$bebida = "guaraná";
+
+echo $refrigerante ;
+
+$destino = "cidade";
+
+$$destino = "ilhés ";
+
+echo $cidade;
